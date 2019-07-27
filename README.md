03eedfb05e0a53f32591a6f4120b1adcbdde711c
Bikal Shrestha <bikal1000.bs1@gmail.com>
2019-07-28 00:00:00 +0545

    Update index.html

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# bikal1000.github.io
-Birthday Gift for someone Special :)
+# Birthday Gift 🎂
+
+An interactive, animated birthday page you can send someone special. Lights turn on, balloons fly in, candles light up, cake gets cut, and a scrolling message plays out — all triggered by a row of buttons at the bottom.
+
+**[Live demo](https://bikal1000.github.io/birthday-gift/)**
+
+## Features
+
+- Twinkling bulb lights
+- Custom name banner and floating letter balloons
+- Animated birthday cake with flickering candles
+- Cut the Cake — candles blow out and the cake splits to reveal a slice
+- Scrolling birthday message
+- Background music
+
+## Getting started
+
+No build step — it's plain HTML/CSS/JS.
+
+```bash
+git clone https://github.com/bikal1000/bikal1000.github.io.git
+cd bikal1000.github.io
+```
+
+Open `index.html` in a browser, or serve it locally:
+
+```bash
+python -m http.server 8000
+```
+
+Then visit `http://localhost:8000`.
+
+## Customizing
+
+- **Name / letters**: edit the `.balloons` divs in [index.html](index.html)
+- **Message**: edit the `<p>` lines inside `.row.message` in [index.html](index.html)
+- **Music**: replace [hbd.mp3](hbd.mp3) with your own track (keep the filename or update the `<source>` tag)
+- **Photo**: replace [images/bd1.jpg](images/bd1.jpg)
+- **Colors / styling**: [css/stylesheet.css](css/stylesheet.css) and [css/cake.less](css/cake.less)
+
+## Project structure
+
+```
+index.html          markup + button sequence
+effect.js            jQuery interactions/animations for each step
+css/stylesheet.css   layout, bulbs, balloons, buttons, cake-cutting
+css/cake.less        cake shape/candle styling
+css/loading.css       loading screen
+images/               banner, balloons, bulbs, photo
+hbd.mp3               background song
+```
+
+## Contributing
+
+Issues and PRs welcome — new animations, mobile fixes, and accessibility improvements are all fair game.
+
+## License
+
+MIT
--- effect.js
+++ effect.js
@@ -6,14 +6,12 @@ $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b1,#b2,#b3,#b4,#b5').stop();
+			$('#b11').animate({top:240, left: vw-250},500);
+			$('#b22').animate({top:240, left: vw-150},500);
+			$('#b33').animate({top:240, left: vw-50},500);
+			$('#b44').animate({top:240, left: vw+50},500);
+			$('#b55').animate({top:240, left: vw+150},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -87,33 +85,16 @@ $('document').ready(function(){
 		});
 	}
 
-	function loopSix() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b6').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSix();
-		});
-	}
-	function loopSeven() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
-	}
-
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
+		$('#b1,#b4,#b5').addClass('balloons-rotate-behaviour-one');
+		$('#b2,#b3').addClass('balloons-rotate-behaviour-two');
 		loopOne();
 		loopTwo();
 		loopThree();
 		loopFour();
 		loopFive();
-		loopSix();
-		loopSeven();
-		
+
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#cake_fadein').fadeIn('slow');
 		});
@@ -129,29 +110,37 @@ $('document').ready(function(){
 	$('#light_candle').click(function(){
 		$('.fuego').fadeIn('slow');
 		$(this).fadeOut('slow').promise().done(function(){
+			$('#cut_cake').fadeIn('slow');
+		});
+	});
+
+	$('#cut_cake').click(function(){
+		// knife descends first; the slice only separates once it lands (40% of 1.7s)
+		$('.cake').addClass('cutting');
+		setTimeout(function(){
+			$('.fuego').fadeOut(250);
+			$('.cake').addClass('cut');
+		},680);
+		$(this).fadeOut('slow').delay(2400).promise().done(function(){
 			$('#wish_message').fadeIn('slow');
 		});
 	});
 
-		
+
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
 
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+		$('#b1,#b2,#b3,#b4,#b5').stop();
 		$('#b1').attr('id','b11');
 		$('#b2').attr('id','b22')
 		$('#b3').attr('id','b33')
 		$('#b4').attr('id','b44')
 		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		$('#b11').animate({top:240, left: vw-250},500);
+		$('#b22').animate({top:240, left: vw-150},500);
+		$('#b33').animate({top:240, left: vw-50},500);
+		$('#b44').animate({top:240, left: vw+50},500);
+		$('#b55').animate({top:240, left: vw+150},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
